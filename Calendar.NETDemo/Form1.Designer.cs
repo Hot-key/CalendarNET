6484f11034bfe9ca5a1d9ba080a9fa9702f1bd79
Hotkey <rhanwnwmf@protonmail.ch>
2019-09-24 11:06:47 +0900

    초기 커밋.

---
+++ Calendar.NETDemo/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Calendar.NETDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calendar1 = new Calendar.NET.Calendar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // calendar1
+            // 
+            this.calendar1.AllowEditingEvents = true;
+            this.calendar1.BackColor = System.Drawing.Color.White;
+            this.calendar1.CalendarDate = new System.DateTime(2019, 4, 24, 13, 16, 0, 0);
+            this.calendar1.CalendarView = Calendar.NET.CalendarViews.Month;
+            this.calendar1.DateHeaderFont = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.calendar1.DayOfWeekFont = new System.Drawing.Font("Arial", 10F);
+            this.calendar1.DaysFont = new System.Drawing.Font("Arial", 10F);
+            this.calendar1.DayViewTimeFont = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
+            this.calendar1.DimDisabledEvents = true;
+            this.calendar1.HighlightCurrentDay = true;
+            this.calendar1.LoadPresetHolidays = false;
+            this.calendar1.Location = new System.Drawing.Point(14, 11);
+            this.calendar1.Name = "calendar1";
+            this.calendar1.ShowArrowControls = true;
+            this.calendar1.ShowDashedBorderOnDisabledEvents = true;
+            this.calendar1.ShowDateInHeader = true;
+            this.calendar1.ShowDisabledEvents = false;
+            this.calendar1.ShowEventTooltips = false;
+            this.calendar1.ShowTodayButton = true;
+            this.calendar1.Size = new System.Drawing.Size(746, 459);
+            this.calendar1.TabIndex = 0;
+            this.calendar1.TodayFont = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
+            this.calendar1.DateCalenderClick += new System.EventHandler(this.Calendar1_DateCalenderClick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(766, 11);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(264, 676);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(685, 476);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(1253, 698);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.calendar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private NET.Calendar calendar1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Calendar.NETDemo/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Calendar.NET;
+
+namespace Calendar.NETDemo
+{
+    public partial class Form1 : Form
+    {
+        [CustomRecurringFunction("RehabDates", "Calculates which days I should be getting Rehab")]
+        private bool RehabDays(IEvent evnt, DateTime day)
+        {
+            if (day.DayOfWeek == DayOfWeek.Monday || day.DayOfWeek == DayOfWeek.Friday)
+            {
+                if (day.Ticks >= (new DateTime(2012, 7, 1)).Ticks)
+                    return false;
+                return true;
+            }
+
+            return false;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            calendar1.CalendarView = CalendarViews.Month;
+            calendar1.AllowEditingEvents = true;
+
+            var groundhogEvent = new CustomEvent()
+                                     {
+                                         Date = new DateTime(2019, 9, 1),
+                                         EventText = "Groundhog Day",
+                                         RecurringFrequency = RecurringFrequencies.Yearly
+                                     };
+
+            calendar1.AddEvent(groundhogEvent);
+        }
+
+        [CustomRecurringFunction("Get Monday and Wednesday", "Selects the Monday and Wednesday of each month")]
+        public bool GetMondayAndWednesday(IEvent evnt, DateTime dt)
+        {
+            if (dt.DayOfWeek == DayOfWeek.Monday || dt.DayOfWeek == DayOfWeek.Wednesday)
+                return true;
+            return false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (var date in calendar1.dateRectangles)
+            {
+                listBox1.Items.Add(date.Key.ToShortDateString() + " " + date.Value.X + ", " + date.Value.Y);
+            }
+        }
+
+        private void Calendar1_DateCalenderClick(object sender, EventArgs e)
+        {
+
+            if (sender is DateTime date)
+            {
+                calendar1.CalendarDate = date;
+                listBox1.Items.Add(calendar1.CalendarDate.ToString("yyyy-MM-dd dddd"));
+            }
+        }
+    }
+}
